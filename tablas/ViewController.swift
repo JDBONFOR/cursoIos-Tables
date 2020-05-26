a2dac54521e7de844c3d369ca2a471a4a5fc5e57
Juan Bonforti <juan@synapticlinks.com.ar>
2020-05-25 21:12:52 -0300

    Initial Commit

---
+++ tablas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tablas
+//
+//  Created by Juan Bonforti on 25/05/2020.
+//  Copyright © 2020 Juan Bonforti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
